0fe61b6234b804bca19c60ee70da40ad4e6df066
KwonChanul <kncnul94@gmail.com>
2018-07-09 16:42:32 +0900

    Counter 수정 - concat -> push로 수정 - 주석 작성

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -7,15 +7,26 @@ class Counter extends Component {
     //     offset: 1
     // }
 
-    // Life Cycle
+    // Life Cycle ( 생성자 )
     constructor(props) {
-        super(props)
-        console.log("Create")
+        super(props);
+        console.log("Create");
         this.state = {
             number: [0],
             offset: 10,
             index:0
-        }
+        };
+        // 증가 함수
+        this.Increase100 = this.handleAddNumber.bind(this, 100);
+        this.Increase10 = this.handleAddNumber.bind(this, 10);
+        this.Increase1 = this.handleAddNumber.bind(this, 1);
+        // 감소 함수
+        this.Decrease100 = this.handleAddNumber.bind(this, -100);
+        this.Decrease10 = this.handleAddNumber.bind(this, -10);
+        this.Decrease1 = this.handleAddNumber.bind(this, -1);
+
+        // this.IncreaseOffset = this.handleAddNumber.bind(this, this.state.offset);
+        // this.DecreaseOffset = this.handleAddNumber.bind(this, -(this.state.offset));
     }
 
     componentWillMount() {
@@ -30,102 +41,92 @@ class Counter extends Component {
         console.log('UnMount');
     }
 
-    handlePutNumber = (offset) => {
-        const { number, index } = this.state
+    // index와 offset이 변해야 re-render
+    shouldComponentUpdate(prevProps,prevState) {
+        console.log(prevState == this.state);
+        if(prevState.index === this.state.index && prevState.offset === this.state.offset) {
+            return false
+        }
+        return true
+    }
+
+    // number 배열에 index의 값에 offset만큼 더한후 추가
+    handleAddNumber = (offset) => {
+        const { number, index } = this.state;
         this.setState({
-            number: (number.length-1 === index ? number : number.slice(0,index+1)).concat(number[index] + offset),
-            index: index + 1
+            // number: ( index === number.length-1 ? number : number.slice(0,index+1)).concat(number[index] + offset),
+            index: (index === number.length-1 ? number : number.slice(0,index+1)).push(number[index] + offset) - 1
+        }, () => {
+            console.log(this.state.index, this.state.number)
         });
     }
 
-
-    //function
-    handleIcrease = () => {
-        const { number, index } = this.state
-        this.handlePutNumber(1)
-        console.log('funtion',index,number[index],number);
-    };
-
-    handleDecrease = () => {
-        this.handlePutNumber(-1)
-    };
-
-    handleIcreaseT = () => {
-        this.handlePutNumber(10)
-    };
-
-    handleDecreaseT = () => {
-        this.handlePutNumber(-10)
-    };
-
-    handleIcreaseH = () => {
-        this.handlePutNumber(100)
-    };
-
-    handleDecreaseH = () => {
-        this.handlePutNumber(-100)
-    };
-
-    handleIcreaseC = () => {
-        const { offset } = this.state
-        this.handlePutNumber(offset)
-    };
-
-    handleDecreaseC = () => {
-        const { offset } = this.state
-        this.handlePutNumber(-(offset))
-    };
-
-    handleDecreIndex = () => {
+    // 이전 계산값 불러오기 ( index 조정 )
+    handleDecreaseIndex = () => {
         const { index } = this.state
-        if(index-1 >= 0) {
+        if(index > 0) {
             this.setState({
                 index: index - 1
             });
         }
     }
 
-    handleIncreIndex = () => {
+    // 이후 계산값 불러오기 ( index 조정 )
+    handleIncreaseIndex = () => {
         const { number, index } = this.state
-        if(index + 1 < number.length) {
+        if(index < number.length - 1) {
             this.setState({
                 index: index + 1
             });
         }
     }
 
+    // offset값 바꾸기
     changeOffset = (evt) => {
         const { value } = evt.target;
-        if((value.replace(/[0-9]/g,'') === "")) {
-            this.setState({offset:Number(value)});
+        if(value.trim() === '') {
+            this.setState({offset: 0});
+        } else {
+            const number = parseInt(value);
+            if (number) {
+                this.setState({offset: Number(number)});
+            }
         }
-
     };
 
 
-    //render
+
+    // render ( setState 이후 실행 )
     render() {
         const { index, number } = this.state;
         console.log('render',index,number[index],number);
+        // const Equation = "Equation : 0";
+        // for(int i; i < number)
         return(
             <div>
                 <h1>카운터</h1>
-                <div><button onClick={this.handleDecreIndex}>⬅</button>
-                    &nbsp;값 : {number[index]}&nbsp;
-                    <button onClick={this.handleIncreIndex}>➡︎</button>
+                <div>
+                    <p>{Equation}</p>
                 </div>
-                <button onClick={this.handleDecreaseH}>-100</button>
-                <button onClick={this.handleDecreaseT}>-10</button>
-                <button onClick={this.handleDecrease}>-1</button>
-                <button onClick={this.handleIcrease}>+1</button>
-                <button onClick={this.handleIcreaseT}>+10</button>
-                <button onClick={this.handleIcreaseH}>+100</button>
+                <div>
+                    <button onClick={this.handleDecreaseIndex}>⬅</button>
+                    &nbsp;값 : {number[index]}&nbsp;
+                    <button onClick={this.handleIncreaseIndex}>➡︎</button>
+                </div>
+                <div>
+                    <button onClick={this.Decrease100}>-100</button>
+                    <button onClick={this.Decrease10}>-10</button>
+                    <button onClick={this.Decrease1}>-1</button>
+                    <button onClick={this.Increase1}>+1</button>
+                    <button onClick={this.Increase10}>+10</button>
+                    <button onClick={this.Increase100}>+100</button>
+                </div>
                 <div>
                     <br/>
                     연산값 :
-                    <input type="text" value={this.state.offset} onChange={this.changeOffset} ></input>
-                    <button onClick={this.handleIcreaseC}>+</button>
-                    <button onClick={this.handleDecreaseC}>-</button>
+                    <input type="text" value={this.state.offset} onChange={this.changeOffset}/>
+                    <button onClick={this.handleAddNumber.bind(this, this.state.offset)}>+</button>
+                    <button onClick={this.handleAddNumber.bind(this, -(this.state.offset))}>-</button>
                 </div>
             </div>
         )
